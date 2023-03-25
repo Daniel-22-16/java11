016981c56f6382e48e4a3a8957ed0dea34a32bda
Daniel-22-16 <1475937@senati.pe>
2023-03-25 10:12:17 -0500

    java12

--- JavaApplication4.java
+++ JavaApplication4.java
@@ -6,7 +6,9 @@ public class JavaApplication4 {
    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese el primer número del rango: ");
-    
+        int primerNumero = scanner.nextInt();
+        System.out.print("Ingrese el segundo número del rango: ");
+        int segundoNumero = scanner.nextInt();
    }    
         
    }
