dc49bfede4b4f340d8c624292a38b126ba9592fe
Daniel-22-16 <1475937@senati.pe>
2023-03-25 10:09:50 -0500

    java12

---
+++ JavaApplication3.java
@@ -0,0 +1,16 @@
+package javaapplication3;
+
+public class JavaApplication3 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Morning, Morning, Morning");
+        System.out.println("\thello, have a good day");
+        System.out.println("\t\toh! thank you");
+        System.out.println("\t\t");
+        System.out.println("\t\t you are great");
+        System.out.println("\t\t this is my pleasure");
+        System.out.println("no,..... you are welcome");
+        
+    }
+}
--- JavaApplication4.java
+++ JavaApplication4.java
@@ -1,15 +1,12 @@
 
 package javaapplication4;
+import java.util.Scanner;
 public class JavaApplication4 {
 
-    public static void main(String[] args){ 
-        // TODO code application logic here
-        for (int i = 1; i <= 999; i++) {
-            if (i % 2 != 0) {
-                System.out.println(i);
-                
-            }
-            
-    }
-    }
-}
+   public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el primer número del rango: ");
+    
+   }    
+        
+   }
