8b1cbdb2a50913d1af8c674cfd96d13d88968177
Daniel-22-16 <1475937@senati.pe>
2023-03-25 09:31:32 -0500

    java11

---
+++ JavaApplication4.java
@@ -0,0 +1,11 @@
+
+package javaapplication4;
+public class JavaApplication4 {
+
+    public static void main(String[] args){ 
+        // TODO code application logic here
+        for (int i = 1; i <= 999; i++) {
+            
+    }
+    }
+}
